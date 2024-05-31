f1bea2edc656c19742fe978d2120104951abfb33
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-05-31 05:57:51 -0700

    server is done

--- seventh_assignment/src/main/java/Server/Server.java
+++ seventh_assignment/src/main/java/Server/Server.java
@@ -1,10 +1,48 @@
+package Server;
+
+import Client.ClientHandler;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 // Server Class
 public class Server {
-    // TODO: Implement the server-side operations
+    private Socket clientSocket;
+    private ServerSocket serverSocket;
 
-    // TODO: Add constructor and necessary methods
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
 
-    public static void main(String[] args) {
-        // TODO: Implement the main method to start the server
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                clientSocket = serverSocket.accept();
+                System.out.println("a new client has connected");
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    private void closeServerSocket() {
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2468);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
